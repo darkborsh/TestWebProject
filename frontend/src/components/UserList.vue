436b73728377ca352d1f8dfdfefa6e23fff4e1b2
darkborsh <n_ragnar@mail.ru>
2022-10-04 17:38:49 +0300

    AddEmp button added

--- frontend/src/components/UserList.vue
+++ frontend/src/components/UserList.vue
@@ -1,10 +1,19 @@
 <template>
   <v-navigation-drawer>
     <v-list>
-      <v-subheader style="font-size: 1.2rem; font-weight: bold">Список сотрудников</v-subheader>
+      <v-subheader style="font-size: 1.2rem; font-weight: bold; user-select: none">Список сотрудников</v-subheader>
+      <v-subheader>
+        <v-btn
+            color="primary"
+            dark
+        >
+          Добавить сотрудника
+        </v-btn>
+      </v-subheader>
       <v-list-item-group
         v-model="selectedItem"
         color="primary"
+        style="margin-top: 5px"
       >
         <v-list-item
           v-for="emp in emps"
@@ -24,7 +33,7 @@ export default {
   data: () => ({
     selectedItem: 0,
     emps: [
-      { fio: 'Real Time Ni' }
+      {fio: "Толстой Лев Николаевич"},
     ],
   }),
 
@@ -34,7 +43,7 @@ export default {
       if (splitedFio.length === 3) {
         return splitedFio[0] + ' ' + splitedFio[1].charAt(0) + '.' + ' ' + splitedFio[2].charAt(0) + '.'
       } else {
-        console.log("Wrong Fio parameters" + emp.fio)
+        console.log("Wrong Fio parameters: " + emp.fio)
         this.selectedItem = null
       }
     },
